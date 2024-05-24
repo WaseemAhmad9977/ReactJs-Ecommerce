53b0cf8c76a3eb691dec0517d39a9fff8b093956
Waseem Ahmad <72740911+WaseemAhmad9977@users.noreply.github.com>
2024-05-24 23:28:38 +0530

    waseem

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import 'remixicon/fonts/remixicon.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Admin from "./components/Admin";
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin" element={<Admin></Admin>}/>
+        <Route path="" element />
+        <Route path="" element />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
